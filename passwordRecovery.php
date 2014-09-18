b034077106de7c2f5a59b98025f6bf1995a64b67
Hans-Jakob Bollinger <contact@webbear.ch>
2014-09-18 19:46:39 +0200

    recovery

--- passwordRecovery.php
+++ passwordRecovery.php
@@ -2,7 +2,7 @@
 // put this in a template and remove it after the reset!
 $admin = $users->get('hjbollinger');
 $admin->setOutputFormatting(false);
-$admin->pass = 'somePassword'; // put in your new password
+$admin->pass = 'yUt4vut3eB'; // put in your new password
 $admin->save();
 
 ?>
