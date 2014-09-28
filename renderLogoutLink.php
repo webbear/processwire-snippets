af3cc30ac4d241f03580ae096a51dbaf8aa40634
Hans Jakob Bollinger <contact@webbear.ch>
2014-09-28 08:33:08 +0200

    logout link added

---
+++ renderLogoutLink.php
@@ -0,0 +1,11 @@
+<?php
+
+// use in template (master/main)<?= renderLogoutLink($user)
+	
+function renderLogoutLink($user){
+	$out='';
+	if ($user->isLoggedin()) {
+		$out .= "<div class='logout-link'><a href={$config->urls->admin}login/logout/'>({$user->name}) - Abmelden</a></div>";
+	}
+	return $out;
+}
