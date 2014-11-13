89dd1df3846a7e4d646cd785501043c62b216ec0
Hans-Jakob Bollinger <contact@webbear.ch>
2014-11-13 08:37:26 +0100

    renderImages option wrap added

--- renderImages.php
+++ renderImages.php
@@ -1,9 +1,12 @@
 <?php
+
+
 function renderImages($images, $options= array()) {
 	$defaults = array(
 		'image_resize' => true,
 		'image_width' =>257,
 		'image_height' =>165,
+		'wrap' => true,
 		'image_wrap' => 'div',
 		'image_wrap_class' => 'img',
 		'limit' => 3
@@ -16,14 +19,15 @@ function renderImages($images, $options= array()) {
 		foreach($images as $image) {
 			$c++;
 			$thumb = ($options['image_resize']) ? $image->size($options['image_width'],$options['image_height']) : $image;
-			$out .= "<".$options['image_wrap']." class='".$options['image_wrap_class']." ".$options['image_wrap_class']."{$c}'><img src='{$thumb->url}' alt='{$image->description}' width='{$thumb->width}' height='{$thumb->height}' /></".$options['image_wrap'].">";
+			$img = "<img src='{$thumb->url}' alt='{$image->description}' width='{$thumb->width}' height='{$thumb->height}' />";
+			if ($options['wrap']) {
+				$out .= "<".$options['image_wrap']." class='".$options['image_wrap_class']." ".$options['image_wrap_class']."{$c}'>{$img}</".$options['image_wrap'].">";
+			} else {
+				$out .= $img;
+			}
 			if($c == $options['limit']) break;
-
 		}
 	}
 
 	return $out;
 }
-
-
-?>
