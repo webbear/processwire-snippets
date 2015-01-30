d844f9a5cb09a69034e8f52a378f9bab9ea67d1e
Hans-Jakob Bollinger <contact@webbear.ch>
2015-01-30 13:46:20 +0100

    preg replace for empty nav lists

--- renderTopNav.php
+++ renderTopNav.php
@@ -3,16 +3,17 @@
  * Render items for placement in Foundation 'top-bar' recursive drop-down navigation
  *
  */
-function renderTopNav(PageArray $items, $options = array(), $level = 0) {
+function renderTopNav(PageArray $items, $options = array(),$level=0) {
 
 	$defaults = array(
 		'tree' => 2, // number of levels it should recurse into the tree
 		'dividers' => false,
 		'current_class' => 'current',
-		'has_dropdown_class' => 'has-dropdown',
+		'has_sublevel_class' => 'has-dropdown',
 		'first_child_class' => 'first-child',
 		'last_child_class' => 'last-child',
-		'dropdown_class' => 'dropdown',
+		'sublevel_class' => 'dropdown',
+		'level_class' => 'level-',
 		'repeat' => false, // whether to repeat items with children as first item in their children nav
 		'excluded_pages' => 'site-map',
 		'excluded_templates' => 'news|events'
@@ -23,6 +24,7 @@ function renderTopNav(PageArray $items, $options = array(), $level = 0) {
 	$page = wire('page');
 	$out = '';
 	$c = 0;
+	$lc = $level + 1;
 	foreach($items as $item) {
 		++$c;
 		$numChildren = $item->numChildren(true);
@@ -32,7 +34,7 @@ function renderTopNav(PageArray $items, $options = array(), $level = 0) {
 		if(in_array($item->template, explode("|", $options['excluded_templates']))) continue;
 		$total = count($items) - count(explode("|",$options['excluded_pages'])) - count(explode('|',$options['excluded_templates']));
 		$class = '';
-		if($numChildren) $class .= $options['has_dropdown_class']." ";
+		if($numChildren) $class .= $options['has_sublevel_class']." ";
 		if($page->id == $item->id) $class .= $options['current_class']." ";
 		if(($item->id > 1 && $page->parents->has($item)) || $page->id == $item->id) $class .= "active ";
 		$class .= " nav-item-".$c;
@@ -44,7 +46,8 @@ function renderTopNav(PageArray $items, $options = array(), $level = 0) {
 		$out .= "$divider<li$class><a href='$item->url'>$item->title</a>";
 
 		if($numChildren) {
-			$out .= "<ul class='{$options['dropdown_class']}'>";
+
+			$out .= "<ul class='{$options['sublevel_class']} {$options['level_class']}{$lc}'>";
 			if($options['repeat']) $out .= "$divider<li><a href='$item->url'>$item->title</a></li>";
 			$out .= renderTopNav($item->children, $options, $level+1);
 			$out .= "</ul>";
@@ -52,6 +55,8 @@ function renderTopNav(PageArray $items, $options = array(), $level = 0) {
 
 		$out .= "</li>";
 	}
-	return str_replace("<ul class='{$options['dropdown_class']}'></ul>", "", $out);
+
+	$pattern = '~<ul[^>]*>(</ul>)~s';
+	return preg_replace($pattern, "", $out);
 	//return $out;
 }
