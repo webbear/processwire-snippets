04b0f9e0f5cbf328dc91e7a017ad243ea71522ce
Hans Jakob Bollinger <contact@webbear.ch>
2019-02-22 08:21:31 +0100

    fix cssClass Function

--- cssClasses.php
+++ cssClasses.php
@@ -1,32 +1,32 @@
 <?php
-function cssClasses() {
-	$out='';
-	$page = wire('page');
-	$language = wire('user')->language->name;
-	$classes = array();
-	
-	//get the segments
-	if ($page->id == 1) {
-		if ($page->path != "/") {
-			$segment1 = str_replace('/','', $page->path);
-			$classes[] = "homepage-$segment1";
-		} else {
-			$classes[] = "homepage";
-		}
-	} else {
-		$segments = array();
-		$segments= explode( " ", trim(str_replace("/", " ", $page->path)));
-		for ($i=0; $i <= count($segments); $i++) {
-			$segment = $segments[$i];
-			$classes[] = (is_numeric(substr($segment,0,1))) ? 'n'.$segment : $segment;
-		}
-	}
-	$classes[] = "page-$page->id";
-	$classes[] = ($page->id != 1) ? "page-$page->name" : "page-home";
-	$classes[] = ($page->rootParent->id != 1) ? "section-{$page->rootParent->name}" : "";
-	$classes[] = "template-" . $page->template->name;
-	$classes[] = "language-" . $language;
+function cssClasses()
+{
+    $out      = '';
+    $page     = wire('page');
+    $language = wire('user')->language->name;
+    $classes  = array();
 
-	$out = implode(' ', $classes);
-	return $out;
+    //get the segments
+    if ($page->id == 1) {
+        if ($page->path != "/") {
+            $segment1  = str_replace('/', '', $page->path);
+            $classes[] = "homepage-$segment1";
+        } else {
+            $classes[] = "homepage";
+        }
+    } else {
+        $segments = array();
+        $segments = explode(" ", trim(str_replace("/", " ", $page->path)));
+        foreach ($segments as $segment) {
+            $classes[] = (is_numeric(substr($segment, 0, 1))) ? 'n' . $segment : $segment;
+        }
+    }
+    $classes[] = "page-$page->id";
+    $classes[] = ($page->id != 1) ? "page-$page->name" : "page-home";
+    $classes[] = ($page->rootParent->id != 1) ? "section-{$page->rootParent->name}" : "";
+    $classes[] = "template-" . $page->template->name;
+    $classes[] = "language-" . $language;
+
+    $out = implode(' ', $classes);
+    return $out;
 }
